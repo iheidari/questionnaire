3b98b4b0fed3cec36dbe67e67c915a234e5cd062
Iman Heidari <iman.heidari@gmail.com>
2018-05-09 23:45:59 -0700

    some basic structor regarding header, footer and base route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/home'
 
 const App = () => {
   return (
     <div className="content">
-      This is a my-cra app
+      <Header />
+      <Router>
+      <div>
+      <Route exact path='/' component={Home} />
+            </div>
+      </Router>
+      <Footer />
     </div>
   );
 };
